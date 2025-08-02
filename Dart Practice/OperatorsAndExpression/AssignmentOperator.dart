4aebb5a40648b83c299c585dd74bd65a82139e3d
LevanceWam <levancewamley@gmail.com>
2025-08-02 14:51:46 -0400

    working on git ignore

---
+++ Dart Practice/OperatorsAndExpression/AssignmentOperator.dart
@@ -0,0 +1,20 @@
+// NOTES:
+/*
+Assignment operators in Dart are used to assign values to variables. 
+They provide a concise way to perform operations and assign the result in a single step. Let's explore the most common assignment operators:
+*/
+
+// Simple assignment (=)
+int a = 5;
+
+// Addition assignment (+=)
+a += 3;  // Equivalent to: a = a + 3
+
+// Subtraction assignment (-=)
+a -= 2;  // Equivalent to: a = a - 2
+
+// Multiplication assignment (*=)
+a *= 4;  // Equivalent to: a = a * 4
+
+// Division assignment (/=)
+a /= 2;  // Equivalent to: a = a / 2
