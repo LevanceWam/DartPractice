c2bdc079ec0b76d0ef3158d488e09d5177e064e7
Levance Wamley Jr. <Levancewamley@gmail.com>
2025-08-02 11:09:16 -0400

    sending a test commit from vscode

---
+++ Dart Practice/VariblesandDataTypes/ArithmeticOperators.dart
@@ -0,0 +1,13 @@
+// NOTES:
+/* 
+Arithmetic operators in Dart allow you to perform basic mathematical operations on numeric values.
+Let's explore the five fundamental arithmetic operators:
+*/
+
+void main(){
+    int a = 10;
+    int b = 3;
+
+
+    int sum = a + b;
+}
